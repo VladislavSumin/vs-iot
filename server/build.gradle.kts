83223b820da70b7cc11d70dc8e8e67b8b6542e39
vladislavsumin <vladislavsumin@mail.ru>
2021-11-12 10:25:39 +0300

    [server] add logging

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,5 +5,10 @@ plugins {
 dependencies {
     implementation(libs.kotlin.coroutines.core)
     implementation(libs.ktor.server.netty)
+
     implementation(libs.logging.kermit)
+    implementation(libs.logging.log4j.api)
+    implementation(libs.logging.log4j.core)
+    implementation(libs.logging.log4j.slf4j)
+    implementation(libs.logging.slf4j)
 }
