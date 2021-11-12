50ae20da2cb678c0aabc255d3355019ecc768779
vladislavsumin <vladislavsumin@mail.ru>
2021-11-13 00:23:43 +0300

    [client-android] add androidXModule

--- client/android/src/main/kotlin/ru/vs/iot/App.kt
+++ client/android/src/main/kotlin/ru/vs/iot/App.kt
@@ -3,7 +3,10 @@ package ru.vs.iot
 import android.app.Application
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import org.kodein.di.android.x.androidXModule
 
 class App : Application(), DIAware {
-    override val di: DI by DI.lazy { }
+    override val di: DI by DI.lazy {
+        import(androidXModule(this@App))
+    }
 }
