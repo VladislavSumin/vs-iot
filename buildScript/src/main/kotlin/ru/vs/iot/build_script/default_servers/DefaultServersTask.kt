3303323a31021150c8dbe4958169fb1281b257d1
vladislavsumin <vladislavsumin@mail.ru>
2021-11-18 20:36:12 +0300

    [defaultServersPlugin] add test class generator

--- buildScript/src/main/kotlin/ru/vs/iot/build_script/default_servers/DefaultServersTask.kt
+++ buildScript/src/main/kotlin/ru/vs/iot/build_script/default_servers/DefaultServersTask.kt
@@ -1,9 +1,16 @@
 package ru.vs.iot.build_script.default_servers
 
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.TypeName
+import com.squareup.kotlinpoet.TypeSpec
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.TaskAction
+
+private const val BASE_PACKAGE = "ru.vs.iot.repository"
+private const val CLASS_NAME = "DefaultServersRepositoryImpl"
 
 abstract class DefaultServersTask : DefaultTask() {
     @get:Nested
@@ -11,5 +18,18 @@ abstract class DefaultServersTask : DefaultTask() {
 
     @TaskAction
     fun action() {
+        generateClass()
+    }
+
+    private fun generateClass() {
+        val clazz = TypeSpec.classBuilder(ClassName(BASE_PACKAGE, CLASS_NAME))
+            .addSuperinterface(ClassName(BASE_PACKAGE, "ServersRepository"))
+            .build()
+
+        val file = FileSpec.builder(BASE_PACKAGE, CLASS_NAME)
+            .addType(clazz)
+            .build()
+
+        file.writeTo(System.out)
     }
 }
