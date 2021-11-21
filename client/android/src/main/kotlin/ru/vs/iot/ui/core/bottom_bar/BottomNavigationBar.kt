5d95e806749ceccfb2687ddd79b0b4136b0ccb58
vladislavsumin <vladislavsumin@mail.ru>
2021-11-21 20:28:55 +0300

    [client:android] setup bottom navigation

--- client/android/src/main/kotlin/ru/vs/iot/ui/core/bottom_bar/BottomNavigationBar.kt
+++ client/android/src/main/kotlin/ru/vs/iot/ui/core/bottom_bar/BottomNavigationBar.kt
@@ -1,13 +1,65 @@
 package ru.vs.iot.ui.core.bottom_bar
 
+import android.graphics.drawable.Icon
 import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.vs.iot.navigation.ui.LocalNavigation
+import ru.vs.iot.navigation.ui.Screen
+import ru.vs.iot.navigation.ui.command.NavigationCommand
+import ru.vs.iot.ui.core.navigation.Servers
+import ru.vs.iot.ui.core.navigation.Settings
 
 @Composable
 fun BottomNavigationBar() {
+    val navigator = LocalNavigation.current
+    val navController = navigator.navController
+    val navBackStackEntry by navigator.navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     BottomNavigation(
         backgroundColor = MaterialTheme.colors.surface
     ) {
+        items.forEach { item ->
+            BottomNavigationItem(
+                selected = currentRoute == item.navigationCommand.route,
+                icon = { Icon(item.icon, "") },
+                onClick = {
+                    navController.navigate(item.navigationCommand.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
     }
+}
+
+private val items = listOf(Item.Servers, Item.Settings)
+
+private sealed class Item(
+    val navigationCommand: NavigationCommand,
+    val icon: ImageVector
+) {
+    object Servers : Item(Screen.Servers, Icons.Filled.Home)
+    object Settings : Item(Screen.Settings, Icons.Filled.Settings)
 }
