c42285348bf5be70de6ae678acf048e28afcfa93
vladislavsumin <vladislavsumin@mail.ru>
2021-11-25 21:12:49 +0300

    [core:navigation] move navigation set to nav module

--- client/android/src/main/kotlin/ru/vs/iot/di/NavigationModule.kt
+++ client/android/src/main/kotlin/ru/vs/iot/di/NavigationModule.kt
@@ -1,9 +1,9 @@
 package ru.vs.iot.di
 
 import org.kodein.di.DI
-import org.kodein.di.bindSet
 import org.kodein.di.inSet
 import org.kodein.di.provider
+import ru.vs.iot.navigation.coreNavigation
 import ru.vs.iot.navigation.ui.Screen
 import ru.vs.iot.navigation.ui.destination.NavigationDestination
 import ru.vs.iot.navigation.ui.destination.createDestination
@@ -13,7 +13,7 @@ import ru.vs.iot.ui.screen.entities.EntititesScreen
 import ru.vs.iot.ui.screen.settings.SettingsScreen
 
 fun Modules.navigation() = DI.Module("navigation") {
-    bindSet<NavigationDestination>()
+    importOnce(Modules.coreNavigation())
     inSet<NavigationDestination> { provider { Screen.Entities.createDestination { EntititesScreen() } } }
     inSet<NavigationDestination> { provider { Screen.Settings.createDestination { SettingsScreen() } } }
 }
