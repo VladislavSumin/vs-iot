f697275d2b1cb4bc14220502ee5e601743172d5b
vladislavsumin <vladislavsumin@mail.ru>
2021-11-25 22:07:38 +0300

    [feature:entities:client] add more design to EntitiesScreen.kt

--- feature/entities/client/src/main/kotlin/ru/vs/iot/entities/ui/EntitiesScreen.kt
+++ feature/entities/client/src/main/kotlin/ru/vs/iot/entities/ui/EntitiesScreen.kt
@@ -1,12 +1,22 @@
 package ru.vs.iot.entities.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ru.vs.iot.compose.di.kodeinViewModel
+import ru.vs.iot.uikit.theme.NONE
+import ru.vs.iot.uikit.theme.Shapes
 
 @Composable
 internal fun EntititesScreen(viewModel: EntitiesViewModel = kodeinViewModel()) {
@@ -17,7 +27,11 @@ internal fun EntititesScreen(viewModel: EntitiesViewModel = kodeinViewModel()) {
 
 @Composable
 private fun RenderEntities(state: EntitiesScreenState.ShowEntities) {
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(0.dp, 8.dp)
+    ) {
         items(state.entities) { entityItem ->
             RenderEntityItem(entityItem)
         }
@@ -26,8 +40,10 @@ private fun RenderEntities(state: EntitiesScreenState.ShowEntities) {
 
 @Composable
 private fun RenderEntityItem(entityItem: EntitiesScreenState.EntityItem) {
-    Column {
-        Text(entityItem.server.name)
-        Text(entityItem.id.raw)
+    Card(modifier = Modifier.fillMaxWidth(), shape = Shapes.NONE) {
+        Column(Modifier.padding(10.dp, 12.dp)) {
+            Text(entityItem.server.name)
+            Text(entityItem.id.raw)
+        }
     }
 }
