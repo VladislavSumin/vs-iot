7239e3c14edecad4369c88a1521ac2a984c46108
vladislavsumin <vladislavsumin@mail.ru>
2021-12-17 22:56:52 +0300

    [rsub] add RSubMessage

--- rsub/core/build.gradle.kts
+++ rsub/core/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
 dependencies {
+    implementation(libs.kotlin.serialization.core)
+    implementation(libs.kotlin.serialization.json)
 }
