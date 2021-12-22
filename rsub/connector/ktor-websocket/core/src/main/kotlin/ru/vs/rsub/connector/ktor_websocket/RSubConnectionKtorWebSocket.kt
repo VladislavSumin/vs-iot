35c431e9767aa00f260ab2fe5a0c08ed518a4030
vladislavsumin <vladislavsumin@mail.ru>
2021-12-22 19:14:51 +0300

    [rSub:client] add check exception on receive

--- rsub/connector/ktor-websocket/core/src/main/kotlin/ru/vs/rsub/connector/ktor_websocket/RSubConnectionKtorWebSocket.kt
+++ rsub/connector/ktor-websocket/core/src/main/kotlin/ru/vs/rsub/connector/ktor_websocket/RSubConnectionKtorWebSocket.kt
@@ -4,10 +4,14 @@ import io.ktor.http.cio.websocket.DefaultWebSocketSession
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.close
 import io.ktor.http.cio.websocket.readText
+import io.ktor.utils.io.core.EOFException
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
 import ru.vs.rsub.RSubConnection
+import ru.vs.rsub.RSubException
+import ru.vs.rsub.RSubExpectedExceptionOnConnectionException
 
 class RSubConnectionKtorWebSocket(
     private val session: DefaultWebSocketSession
@@ -16,6 +20,15 @@ class RSubConnectionKtorWebSocket(
         get() = session.incoming.receiveAsFlow()
             .map { it as Frame.Text }
             .map { it.readText() }
+            .catch { exception ->
+                throw when (exception) {
+                    is EOFException -> RSubExpectedExceptionOnConnectionException(
+                        exception.message ?: "Expected exception while receiving messages",
+                        exception
+                    )
+                    else -> RSubException("Unknown exception while receiving message", exception)
+                }
+            }
 
     override suspend fun send(data: String) = session.send(Frame.Text(data))
 
