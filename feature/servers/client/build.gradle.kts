e3155d9690d254696eae96d7d50c3a1ff4ec8bb2
vladislavsumin <vladislavsumin@mail.ru>
2022-01-29 12:38:50 +0300

    [feature:servers] cleanup build.gradle.kts

--- feature/servers/client/build.gradle.kts
+++ feature/servers/client/build.gradle.kts
@@ -17,15 +17,14 @@ kotlin {
         named("commonMain") {
             dependencies {
                 implementation(project(":core:coroutines"))
-                api(project(":core:decompose"))
+                implementation(project(":core:decompose"))
                 implementation(project(":core:di"))
-//                api(project(":core:logging"))
-                api(project(":core:navigation2"))
-//                api(project(":core:settings"))
+                implementation(project(":core:ktor-client"))
+                implementation(project(":core:logging"))
+                implementation(project(":core:navigation2"))
                 implementation(project(":core:uikit"))
                 implementation(project(":core:utils"))
 
-                implementation(libs.ktor.client.core)
                 implementation(libs.sqldelight.coroutines)
 
                 implementation(project(":feature:servers:dto"))
@@ -33,21 +32,3 @@ kotlin {
         }
     }
 }
-
-// dependencies {
-//    // implementation(project(":core:autostart"))
-//    implementation(project(":core:compose"))
-//    implementation(project(":core:coroutines"))
-//    implementation(project(":core:di"))
-//    implementation(project(":core:navigation"))
-//    implementation(project(":core:uikit"))
-//    implementation(project(":core:utils"))
-//
-//    implementation(project(":feature:servers:dto"))
-//    // implementation(project(":feature:default-servers"))
-//
-//    implementation(libs.sqldelight.coroutines)
-//    implementation(libs.sqldelight.android)
-//
-//    implementation(libs.ktor.client.android)
-// }
