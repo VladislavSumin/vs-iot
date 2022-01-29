0d9cc5baf6f9d11a627d8197e386bc3f58f34f59
vladislavsumin <vladislavsumin@mail.ru>
2022-01-29 20:26:46 +0300

    [client:jvm] add onBackPress on press Escape

--- client/jvm/src/jvmMain/kotlin/ru/vs/iot/Main.kt
+++ client/jvm/src/jvmMain/kotlin/ru/vs/iot/Main.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.ScrollbarStyle
 import androidx.compose.foundation.defaultScrollbarStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import co.touchlab.kermit.Logger
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
+import com.arkivanov.essenty.backpressed.BackPressedDispatcher
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import org.kodein.di.DI
 import org.kodein.di.compose.withDI
@@ -19,6 +23,7 @@ import ru.vs.iot.di.clientCommon
 import ru.vs.iot.logging.setupDefault
 import ru.vs.iot.ui.RootUi
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     Logger.setupDefault()
 
@@ -27,7 +32,8 @@ fun main() {
     }
 
     val lifecycle = LifecycleRegistry()
-    val defaultComponentContext = DefaultComponentContext(lifecycle)
+    val backPressedDispatcher = BackPressedDispatcher()
+    val defaultComponentContext = DefaultComponentContext(lifecycle, backPressedHandler = backPressedDispatcher)
 
     application {
         val windowState = rememberWindowState()
@@ -36,7 +42,14 @@ fun main() {
         Window(
             onCloseRequest = this::exitApplication,
             state = windowState,
-            title = "vs-iot"
+            title = "vs-iot",
+            onKeyEvent = {
+                if (it.key == Key.Escape) {
+                    backPressedDispatcher.onBackPressed()
+                } else {
+                    false
+                }
+            }
         ) {
             withDI(di) {
                 DesktopScrollbarStyle {
